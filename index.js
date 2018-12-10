a2babd892a00b397596f14387213a191c8ee6529
github username <github email address>
2018-12-09 23:53:18 -0500

    models and auth

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/Users');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -11,4 +16,3 @@ require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-console.log('test')
--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,28 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  console.log('access token', accessToken);
-  console.log('refreshToken', refreshToken);
-  console.log('profile:', profile);
+  // console.log('access token', accessToken);
+  // console.log('refreshToken', refreshToken);
+  // console.log('profile:', profile);
+  User.findOne({ googleId: profile.id })
+    .then((existingUser) => {
+      if (existingUser) {
+        // we already have a record with the profile id
+        done(null, existingUser);
+      } else {
+        new User({ googleId: profile.id }).save()
+          .then(user => done(null, user));
+      }
+    });
+
   })
 );
